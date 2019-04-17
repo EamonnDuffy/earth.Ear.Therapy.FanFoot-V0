27bcf156f500c8a7bae562e83b33247a03ade587
Éamonn Duffy <Eamonn@Duffy.global>
2019-04-17 21:11:51 +0100

    Fleshed out BaseRepository.

--- Earth.Ear.Therapy.FanFoot/Controllers/HomeController.cs
+++ Earth.Ear.Therapy.FanFoot/Controllers/HomeController.cs
@@ -1,18 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Earth.Ear.Therapy.FanFoot.DataAccess.EntityFramework.Repositories.FanFootTherapy;
+using Earth.Ear.Therapy.FanFoot.Models;
+using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
-'using Earth.Ear.Therapy.FanFoot.DataAccess.EntityFramework.Databases;
-using Earth.Ear.Therapy.FanFoot.DataAccess.EntityFramework.Repositories.FanFootTherapy;
-using Microsoft.AspNetCore.Mvc;
-using Earth.Ear.Therapy.FanFoot.Models;
+using Earth.Ear.Therapy.FanFoot.DataAccess.EntityFramework.Entities.FanFootTherapy;
 
 namespace Earth.Ear.Therapy.FanFoot.Controllers
 {
     public class HomeController : Controller
     {
         private IDatabaseVersionsRepository DatabaseVersionsRepository { get; }
+
         public HomeController(IDatabaseVersionsRepository databaseVersionsRepository)
         {
             DatabaseVersionsRepository = databaseVersionsRepository;
@@ -22,6 +21,12 @@ namespace Earth.Ear.Therapy.FanFoot.Controllers
         {
 #if true
             var one = DatabaseVersionsRepository.Get(1);
+
+            List<DatabaseVersionEntity> entities = null;
+
+            entities = DatabaseVersionsRepository.GetAll().ToList();
+
+            entities = DatabaseVersionsRepository.GetMany(entity => entity.Major == 0).ToList();
 #endif
 
             return View();
--- global.Duffy.DataAccess/EntityFramework/Repositories/BaseRepository.cs
+++ global.Duffy.DataAccess/EntityFramework/Repositories/BaseRepository.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using global.Duffy.DataAccess.EntityFramework.Databases;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,7 +8,21 @@ namespace global.Duffy.DataAccess.EntityFramework.Repositories
 {
     public interface IBaseRepository<TDatabase, TEntity, TEntityIdType>
     {
+        void Create(TEntity entity);
+
         TEntity Get(TEntityIdType entityId);
+
+        TEntity GetFirstOrDefault(Func<TEntity, bool> where);
+
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> GetMany(Func<TEntity, bool> where);
+
+        void Update(TEntity entity);
+
+        void Delete(TEntity entity);
+
+        int SaveChanges();
     }
 
     public class BaseRepository<TDatabase, TEntity, TEntityIdType> : IBaseRepository<TDatabase, TEntity, TEntityIdType>
@@ -16,6 +32,11 @@ namespace global.Duffy.DataAccess.EntityFramework.Repositories
     {
         private TDatabase Database { get; }
 
+        public void Create(TEntity entity)
+        {
+            Database.Context.Set<TEntity>().Add(entity);
+        }
+
         public BaseRepository(TDatabase database)
         {
             Database = database;
@@ -24,6 +45,36 @@ namespace global.Duffy.DataAccess.EntityFramework.Repositories
         public TEntity Get(TEntityIdType entityId)
         {
             return Database.Context.Set<TEntity>().Find(entityId);
+        }
+
+        public TEntity GetFirstOrDefault(Func<TEntity, bool> where)
+        {
+            return Database.Context.Set<TEntity>().FirstOrDefault(where);
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Database.Context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> GetMany(Func<TEntity, bool> where)
+        {
+            return Database.Context.Set<TEntity>().Where(where);
+        }
+
+        public void Update(TEntity entity)
+        {
+            Database.Context.Set<TEntity>().Update(entity);
+        }
+
+        public void Delete(TEntity entity)
+        {
+            Database.Context.Set<TEntity>().Remove(entity);
+        }
+
+        public int SaveChanges()
+        {
+            return Database.SaveChanges();
         }
     }
 }
