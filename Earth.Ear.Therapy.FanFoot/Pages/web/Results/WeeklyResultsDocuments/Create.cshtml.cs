19c07a18379dd22000a14ef845a07a162694e448
Éamonn Duffy <Eamonn@Duffy.global>
2019-04-26 18:14:18 +0100

    Added preliminary Weekly Results Sets and Documents web support.

---
+++ Earth.Ear.Therapy.FanFoot/Pages/web/Results/WeeklyResultsDocuments/Create.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Earth.Ear.Therapy.FanFoot.Pages.web.WeeklyResultsDocuments
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ Earth.Ear.Therapy.FanFoot/Pages/web/Results/WeeklyResultsSets/Create.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Earth.Ear.Therapy.FanFoot.Pages.web.WeeklyResultsSets
+{
+    public class CreateModel : PageModel
+    {
+        private IActionResult CreateResultsSet()
+        {
+            return new JsonResult(new { });
+        }
+
+        public IActionResult OnGet(bool ajaxCallBack = false)
+        {
+            IActionResult actionResult = null;
+
+            if (ajaxCallBack)
+            {
+                actionResult = CreateResultsSet();
+            }
+            else
+            {
+                actionResult = Page();
+            }
+
+            return actionResult;
+        }
+    }
+}
